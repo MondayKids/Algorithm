32881a5d58442067bcf3ba61d586522d64b52277
jaehui327 <jaeheekim970327@gmail.com>
2023-09-18 09:51:16 +0900

    [23-09-18] JaeHui - [카드 정렬하기]

---
+++ JaeHui/23-09/18/Main_B_1715_카드정렬하기.java
@@ -0,0 +1,28 @@
+package BOJ;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_B_1715_카드정렬하기 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < N; i++) pq.offer(Integer.parseInt(br.readLine()));
+
+        int answer = 0;
+        int fir, sec;
+        while(pq.size() > 1) {
+            fir = pq.poll();
+            sec = pq.poll();
+
+            answer += fir + sec;
+            pq.add(fir + sec);
+        }
+
+        System.out.println(answer);
+        br.close();
+    }
+}
