cd231866efb224bc0f4c42b9eb206210de5393e5
jaehui327 <jaeheekim970327@gmail.com>
2023-09-24 00:12:10 +0900

    [23-09-23] JaeHui - [두 용액]

---
+++ JaeHui/23-09/23/Main_B_2470_두용액.java
@@ -0,0 +1,39 @@
+package BOJ;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_B_2470_두용액 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        long[] arr = new long[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++)
+            arr[i] = Long.parseLong(st.nextToken());
+        Arrays.sort(arr);
+
+        int left = 0;
+        int right = N - 1;
+        long[] answer = new long[2];
+        long min = Long.MAX_VALUE;
+        long sum;
+
+        while(left < right) {
+            sum = arr[left] + arr[right];
+            if (Math.abs(sum) < min) {
+                min = Math.abs(sum);
+                answer[0] = arr[left];
+                answer[1] = arr[right];
+            }
+            if (sum < 0) left++;
+            else right--;
+        }
+        sb.append(answer[0]).append(" ").append(answer[1]);
+        System.out.println(sb);
+        br.close();
+    }
+}
