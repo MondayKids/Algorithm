beab3684fb8cbc42af5b4cd0365f35471441b7c6
KimDongEon <gold3987@naver.com>
2023-10-19 17:57:41 +0900

    [23-10-19] DongEon - [1998...]

---
+++ DongEon/23-10/11~20/19/B18108.java
@@ -0,0 +1,15 @@
+package baekjoon.backjoon10.day1120.day19;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B18108 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int y = Integer.parseInt(br.readLine());
+        System.out.println(y - 543);
+    }
+
+}
